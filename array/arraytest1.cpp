2c143706a1854a600096f0a35f4155ff4e98bfc7
MP <morteza.pourmehdi.se@gmailcom>
2025-05-07 18:41:57 +0100

    vector erase

--- array/arraytest1.cpp
+++ array/arraytest1.cpp
@@ -1,13 +1,79 @@
-#include <array>
-#include <iterator>
-#include <algorithm> // Sort algorithm
-//////////////////////
+
 #include <iostream>
 #include <iterator>
 #include <vector>
 
 using namespace std;
 
+// data
+void main()
+{
+	vector<int> v{ 1,2,3,4,5,6,7,8 };
+	//v.erase(v.end()-1);
+	v.erase(v.begin()+1, v.end()-2);
+	for (int i = 0; i<v.size(); i++)
+		cout << v[i] << endl;
+}
+
+/*
+// data
+void main()
+{
+	vector <int> v(6);
+	vector <int> v2{ 6 };
+
+	int* p;
+	p = v.data();
+
+	*p = 1;
+	p[3] = 9;
+
+	++p;
+	*p = 8;
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v.at(i) << " ";
+
+	cout << endl;
+
+	for (int i = 0; i < v2.size(); i++)
+		cout << v2.at(i) << " ";
+}
+*/
+
+/*
+// reserve, capacity
+void main()
+{
+	vector<int> v; //C++ Standard Library’s std::vector class template.it is a dynamic array that can grow or shrink in size, defined in the <vector> header.
+	vector<int>:: size_type k;// size_type is a nested type (or typedef) defined inside the std::vector class.
+	//It represents an unsigned integer type used for indexing and counting elements in a vector.
+	//size_type is guaranteed to be large enough to represent any valid index or size of the vector.
+	//It’s accessed using the scope resolution operator :: because it’s a member of the vector<int> class.
+	//This specifies the size_type for a vector of int elements.Using vector<int>::size_type ensures portability
+	// and type safety, as it matches the vector’s implementation-specific requirements for sizes and indices.
+	//Why Use size_type Instead of int or unsigned int?
+	//Portability: The size of a vector or its indices might exceed the range of int or unsigned int on some platforms.size_type is guaranteed to be appropriate.
+	//Compatibility : vector::size() returns a size_type, so using size_type for variables avoids type mismatches or warnings(e.g., comparing int with size_t).
+	//Standard Practice : It’s idiomatic in C++ to use size_type when working with std::vector for consistency and correctness.
+	k = v.capacity(); //k=0
+
+	for (int i=1; i<=5 ; i++)
+	{
+		v.push_back(i);
+		if (k != v.capacity())
+		{
+			k = v.capacity();
+			cout << k << endl;
+		}
+	}
+
+
+}
+*/
+//resize() //////////////////////////////////////////////////////
+
+/*
 void main()
 {
 	vector <int> v;
@@ -22,7 +88,7 @@ void main()
 	v.resize(4);
 
 	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << "  ";	
+		cout << v[i] << "  ";
 
 	cout << endl;
 	v.resize(9);
@@ -36,10 +102,9 @@ void main()
 	for (int i = 0; i < v.size(); i++)
 		cout << v[i] << "  ";
 }
+*/
 
-
-
-////////////////////////////////////////////////////////
+//push_back() //////////////////////////////////////////////////////
 /*
 void main()
 {
@@ -80,5 +145,4 @@ void main()
 		cout << v[i] << " ";
 	}
 }
-
 */
